4bc1536d923f03c0b58b7b1e73d3ecdffb098500
tGodson <tendongze95@gmail.com>
2020-10-07 23:37:46 +0100

    add login which checks if cell has already been clicked else continue game flow

--- script.js
+++ script.js
@@ -43,8 +43,31 @@ function handlePlayerChange() {
 function handleResultValidation() {
 
 }
-function handleCellClick() {
-
+function handleCellClick(clickedCellEvent) {
+  /*
+  We will save the clicked html element in a variable for easier further use
+  */    
+      const clickedCell = clickedCellEvent.target;
+  /*
+  Here we will grab the 'data-cell-index' attribute from the clicked cell to identify where that cell is in our grid. 
+  Please note that the getAttribute will return a string value. Since we need an actual number we will parse it to an 
+  integer(number)
+  */
+      const clickedCellIndex = parseInt(
+        clickedCell.getAttribute('data-cell-index')
+      );
+  /* 
+  Next up we need to check whether the call has already been played, 
+  or if the game is paused. If either of those is true we will simply ignore the click.
+  */
+      if (gameState[clickedCellIndex] !== "" || !gameActive) {
+          return;
+      }
+  /* 
+  If everything if in order we will proceed with the game flow
+  */    
+      handleCellPlayed(clickedCell, clickedCellIndex);
+      handleResultValidation();
 }
 function handleRestartGame() {
 
