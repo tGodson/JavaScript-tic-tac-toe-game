2f12a8cc48e175313a6f46fdbc16cb2af40008dc
tGodson <tendongze95@gmail.com>
2020-10-07 23:44:17 +0100

    check wheather the game ended in a win or draw

--- script.js
+++ script.js
@@ -42,12 +42,44 @@ function handleCellPlayed(clickedCell, clickedCellIndex) {
       gameState[clickedCellIndex] = currentPlayer;
       clickedCell.innerHTML = currentPlayer;
 }
+
 function handlePlayerChange() {
 
 }
-function handleResultValidation() {
 
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+function handleResultValidation() {
+  let roundWon = false;
+  for (let i = 0; i <= 7; i++) {
+      const winCondition = winningConditions[i];
+      let a = gameState[winCondition[0]];
+      let b = gameState[winCondition[1]];
+      let c = gameState[winCondition[2]];
+      if (a === '' || b === '' || c === '') {
+          continue;
+      }
+      if (a === b && b === c) {
+          roundWon = true;
+          break
+      }
+  }
+  if (roundWon) {
+      statusDisplay.innerHTML = winningMessage();
+      gameActive = false;
+      return;
+  }
 }
+
 function handleCellClick(clickedCellEvent) {
   /*
   We will save the clicked html element in a variable for easier further use
@@ -74,6 +106,7 @@ function handleCellClick(clickedCellEvent) {
       handleCellPlayed(clickedCell, clickedCellIndex);
       handleResultValidation();
 }
+
 function handleRestartGame() {
 
 }
