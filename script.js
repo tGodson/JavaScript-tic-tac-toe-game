0a9e629a63ba2f35da4203ce984985853b17a3dd
tGodson <tendongze95@gmail.com>
2020-10-09 11:57:47 +0100

    haddle input form for player name

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
+const hideBoard = (() => {
+  document.querySelector(".board").style.display = "none";
+})();
 
 const statusDisplay = document.querySelector('.game--status');
 
+let players = '';
 let gameActive = true;
 let currentPlayer = 'X';
 let gameState = ['', '', '', '', '', '', '', '', ''];
 
-const winningMessage = () => `Player ${currentPlayer} has won!`;
+const winningMessage = () => {
+  if (currentPlayer === 'X'){
+    statusDisplay.innerHTML = `Player ${players.player1} has won!`;
+  }else{
+    statusDisplay.innerHTML = `Player ${players.player2} has won!`;
+  }
+  
+}
 const drawMessage = () => 'Game ended in a draw!';
-const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
-statusDisplay.innerHTML = currentPlayerTurn();
-function handleCellPlayed(clickedCell, clickedCellIndex) {
+const current_player = () => {
+  players = JSON.parse(localStorage.getItem('players'));
+  if (currentPlayer === 'X'){
+    statusDisplay.innerHTML =`It's ${players.player1}'s turn`;
+  }else{
+    statusDisplay.innerHTML = `It's ${players.player2}'s turn`;
+  }
+}
+
+//statusDisplay.innerHTML = current_player(player);
+const handleCellPlayed = (clickedCell, clickedCellIndex) => {
   gameState[clickedCellIndex] = currentPlayer;
   clickedCell.innerHTML = currentPlayer;
 }
 
 const handlePlayerChange = () => {
   currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-  statusDisplay.innerHTML = currentPlayerTurn();
+  if (currentPlayer === 'X'){
+    statusDisplay.innerHTML =`It's ${players.player1}'s turn`;
+  }else{
+    statusDisplay.innerHTML = `It's ${players.player2}'s turn`;
+  }
 };
 
 const winningConditions = [
@@ -31,7 +54,7 @@ const winningConditions = [
   [2, 4, 6],
 ];
 
-const handleResultValidation = () => {
+const gameBoard = () => {
   let roundWon = false;
   for (let i = 0; i <= 7; i += 1) {
     const winCondition = winningConditions[i];
@@ -40,13 +63,14 @@ const handleResultValidation = () => {
     const c = gameState[winCondition[2]];
 
     if (a === '' || b === '' || c === '') {
+      roundWon = false;
     } else if (a === b && b === c) {
       roundWon = true;
       break;
     }
   }
   if (roundWon) {
-    statusDisplay.innerHTML = winningMessage();
+    winningMessage();
     gameActive = false;
     return;
   }
@@ -62,7 +86,7 @@ const handleResultValidation = () => {
   handlePlayerChange();
 };
 
-const handleCellClick = (clickedCellEvent) => {
+const game = (clickedCellEvent) => {
   const clickedCell = clickedCellEvent.target;
   const clickedCellIndex = parseInt(
     clickedCell.getAttribute('data-cell-index'), 10,
@@ -72,16 +96,48 @@ const handleCellClick = (clickedCellEvent) => {
   }
 
   handleCellPlayed(clickedCell, clickedCellIndex);
-  handleResultValidation();
+  gameBoard();
 };
 
 const handleRestartGame = () => {
+  //gameActive = true;
+  //currentPlayer = 'X';
+  //gameState = ['', '', '', '', '', '', '', '', ''];
+  //statusDisplay.innerHTML = current_player();
+  // document.querySelectorAll('.cell')
+  //   .forEach(cell => { cell.innerHTML = ''; });
+    localStorage.removeItem('players');
+    window.location.reload();
+};
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', game));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
+
+const empty = () => {
+  document.querySelector('#player1').value = '';
+  document.querySelector('#player2').value = '';
+}
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  player1 = document.querySelector('#player1').value;
+  player2 = document.querySelector('#player2').value;
+  players = {
+    player1: player1,
+    player2: player2
+  }
+  
+  localStorage.setItem('players', JSON.stringify(players));
+  document.querySelector(".board").style.display = "block";
+  current_player();
+  
   gameActive = true;
   currentPlayer = 'X';
   gameState = ['', '', '', '', '', '', '', '', ''];
-  statusDisplay.innerHTML = currentPlayerTurn();
+  current_player();
   document.querySelectorAll('.cell')
     .forEach(cell => { cell.innerHTML = ''; });
-};
-document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
-document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
+  
+  empty();
+});
+//console.log(JSON.parse(localStorage.getItem('players')));
